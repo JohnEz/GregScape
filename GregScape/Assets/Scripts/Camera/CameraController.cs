d9c82695c2bd02d3d70a6239a6488948e1052ccb
JohnEz <andrew_nomie@msn.com>
2016-04-23 21:00:28 +0100

    Added unityTiled, basic camera, basic character

---
+++ GregScape/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	const float MOVESPEED = 12; //speed the camera moves
+	const float ZOOMSPEED = 0.5f; //speed the camera zooms
+	const float MINZOOM = 2f; //minimum zoom value
+	const float MAXZOOM = 8f; //maximum zoom value
+
+	public float screenWidth = 0;
+	public float screenHeight = 0;
+
+	public bool movingToDestination = false;
+	Vector3 targetLocation;
+
+	public float minX = 0;
+	public float minY = 0;
+	public float maxX = 10;
+	public float maxY = 10;
+
+	public GameObject followTarget;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void Initialise() {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+	}
+
+	//updates the position of the camera via keyboard input
+	void UpdateMovement() {
+
+	}
+
+	void UpdateZoom() {
+		float d = Input.GetAxis("Mouse ScrollWheel");
+
+
+		Camera cam = GetComponent<Camera> ();
+
+		if (d > 0f)
+		{
+			//zoom in
+			cam.orthographicSize -= ZOOMSPEED;
+
+			if (cam.orthographicSize < MINZOOM) {
+				cam.orthographicSize = MINZOOM;
+			}
+		}
+		else if (d < 0f)
+		{
+			//zoom out
+			cam.orthographicSize += ZOOMSPEED;
+			
+			if (cam.orthographicSize > MAXZOOM) {
+				cam.orthographicSize = MAXZOOM;
+			}
+		}
+	}
+
+
+	public void MoveToTarget(Vector3 pos) {
+		movingToDestination = true;
+		targetLocation = new Vector3 (pos.x, pos.y, transform.position.z);
+	}
+}
---
+++ GregScape/Assets/Scripts/Player/playerControler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+
+	void Movement () {
+
+		Vector2 direction = new Vector2 ();
+
+		if (Input.GetKey (KeyCode.D)) {
+			direction.x += 1;
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			direction.x -= 1;
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			direction.y += 1;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			direction.y -= 1;
+		}
+
+		direction.Normalize ();
+
+		direction *= 0.1f;
+
+		transform.position += new Vector3(direction.x, direction.y, 0);
+	}
+}
